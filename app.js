f296fdb5c78a845074f56202dd658a3150ff0f64
muhammad ashir noman <141489855+ashirnoman00@users.noreply.github.com>
2023-09-08 02:03:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,133 @@
+                        //CHAPTER # 5
+                                              //MATH EXPRESSIONS
+
+//! Q# 1
+//  a = 5
+//  b = 4
+//  var sum = a+b
+// document.write("sum of 5 and 4 is " + sum)
+
+//! Q# 2
+//?FOR SUBTRACTION
+// a = 5 
+// b = 4
+// var subtract = a-b
+// document.write("subtraction of 5 and 4 is " + subtract)
+
+//? FOR MULTIPLICATION
+// a = 5 
+// b =  4
+// var multiplication  = a*b
+// document.write("multiplication of 5 and 4 is "+ multiplication )
+
+//? FOR DIVISION
+// a = 5 
+// b = 4
+// var division = a/b
+// document.write("division of a and b is " + division)
+
+//? FOR MODULUS
+// a = 5
+// b =  4
+// var modulus = a%b
+// document.write("modulus of a and b is " + modulus)
+
+//!Q # 3
+//  var x = 
+//  document.write("Value after variable declaration is: "+ x)
+
+// var number = 5
+// document.write("initial value: "+ number)
+
+// var number = 6
+// ++number
+// document.write("value after increment is: " + number)
+
+// var number = 6+7
+// document.write("value after addition is "+ number)
+
+// var number = 13
+// --number
+//  document.write("value after decrement is :"+ number)
+
+// var number = 12%3
+// document.write("The remainder is :" + number )
+
+//! Q # 4
+// var price = 600
+// var numberoftickets = 5
+// var cost = 600*5 
+// document.write("The total cost to buy 5 tickets to a movie is: "+  cost +"PKR")
+
+//? Q# 5 NOT INCLUDED!!
+
+//! Q# 6
+// Part A: Store a Celsius temperature into a variable
+// var celsiusTemperature = 30;
+
+// Part B: Convert Celsius to Fahrenheit and output the result
+// var fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+// document.write(celsiusTemperature + "°C is " + fahrenheitTemperature + "°F");
+
+// Part C: Store a Fahrenheit temperature into a variable
+// var fahrenheitTemperature2 = 86; 
+
+// Part D: Convert Fahrenheit to Celsius and output the result
+// var celsiusTemperature2 = (fahrenheitTemperature2 - 32) * 5/9;
+// document.write(fahrenheitTemperature2 + "°F is " + celsiusTemperature2 + "°C");
+
+//! Q# 7
+// var price_of_item_1 = 650
+// var price_of_item_2 = 100
+// var quantity_of_item_1 = 3
+// var quantity_of_item_2 = 7
+// var shipping_charges = 100
+//  var quantity_1  = 3*650
+//  var quantity_2  = 7*100
+//  var total_cost = 2650+100
+//  document.write("Total cost of your order is "+ total_cost )
+
+//! Q# 8
+// var marks_obtained_in_english = 97;
+// var marks_obtained_in_maths = 80;
+// var total_marks = 200;
+// var total_obtained_marks = marks_obtained_in_english + marks_obtained_in_maths;
+// var percentage = (total_obtained_marks / total_marks) * 100;
+// document.write("Total marks: " + total_marks + "<br> marks obtained: " + total_obtained_marks + "<br> percentage: " + percentage);
+
+//Q# 9
+// var us_dollar = 10
+// var saudi_riyal = 25
+// var exchange = 10*308
+// var exchange_2 = 25*28
+// var exchange_rates = 3780
+// document.write("Total Currency in PKR "+ exchange_rates)
+
+// Q# 10
+// var number = 20; 
+
+// var result = ((number + 5) * 10) / 2;
+
+// document.write("Result:", result);
+
+//! Q# 11
+var current_year = 2023
+var birthday_year =  2003
+var possible_age  =  current_year-birthday_year
+document.write("Current year: "+current_year+"<br> Birthday year:" +birthday_year+"<br> Your Age is "+ possible_age)
+
+//! Q# 12
+// var radius = 20
+// var circumference = 2*3.14*radius
+// var area = 3.14*radius*radius
+// document.write("Radius of circle: "+radius +"<br> The Circumference is: "+ circumference+"<br> The area is:"+ area)
+
+//Q# 13
+// var favoriteSnack = "Lays";
+// var currentAge = 21;
+// var maximumAge = 80;
+// var snacksPerDay = 3;
+
+// var snacksNeeded = (maximumAge - currentAge) * 365 * snacksPerDay;
+
+// document.write(`Favorite snack: ${favoriteSnack}<br>Current age: ${currentAge}<br>Estimated maximum age: ${maximumAge}<br>Amount of snacks per day: ${snacksPerDay}<br>You will need ${snacksNeeded} ${favoriteSnack} to last you until the ripe old age of ${maximumAge}.`);
